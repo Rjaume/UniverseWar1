9d4b732071363045394cf616b8df560767e8c85f
Rjaume <68929395+Rjaume@users.noreply.github.com>
2022-05-08 22:07:30 +0200

    Delete src/joc directory

--- src/joc/Objecte.java
+++
@@ -1,8 +0,0 @@
-package joc;
-
-public class Objecte {
-int x,y;
-int llargada,altura; //també podria fer que nau no extengues objecte així podría tenir aquestes variables static (no sé si és millor en termes de rendiment)
-int llargadaMinimapa,alturaMinimapa;
-boolean tracked; //si el trackejem amb el radar o no
-}
